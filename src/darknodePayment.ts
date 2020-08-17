2747703d29b1ee3db9f7651801c3f4998a9912e7
noiach <gh@noiach.com>
2020-08-17 15:21:37 +1000

    fix: add error catching to balance fetching

--- src/darknodePayment.ts
+++ src/darknodePayment.ts
@@ -2,7 +2,11 @@ import { Address } from "@graphprotocol/graph-ts";
 
 import { Gateway } from "../generated/BTCGateway/Gateway";
 import {
-    ChangeCycleCall, DarknodePayment, LogDarknodeClaim, LogDarknodeWithdrew, WithdrawCall,
+    ChangeCycleCall,
+    DarknodePayment,
+    LogDarknodeClaim,
+    LogDarknodeWithdrew,
+    WithdrawCall,
 } from "../generated/DarknodePayment/DarknodePayment";
 import { bchGateway, btcGateway, zecGateway } from "./_config";
 import { getDarknode, getRenVM } from "./common";
@@ -39,9 +43,18 @@ export function handleWithdraw(call: WithdrawCall): void {
     let darknodeID = call.inputs._darknode;
     let darknode = getDarknode(darknodeID);
 
-    darknode.balanceBTC = darknodePayment.darknodeBalances(Address.fromHexString(darknodeID.toHexString()) as Address, BTCGateway.token())
-    darknode.balanceZEC = darknodePayment.darknodeBalances(Address.fromHexString(darknodeID.toHexString()) as Address, ZECGateway.token())
-    darknode.balanceBCH = darknodePayment.darknodeBalances(Address.fromHexString(darknodeID.toHexString()) as Address, BCHGateway.token())
+    let darknodeAddress: Address = Address.fromHexString(darknodeID.toHexString()) as Address;
+
+    let btcToken = BTCGateway.token();
+    let zecToken = ZECGateway.token();
+    let bchToken = BCHGateway.token();
+
+    let btcCall = darknodePayment.try_darknodeBalances(darknodeAddress, btcToken);
+    if (!btcCall.reverted) { darknode.balanceBTC = btcCall.value; }
+    let zecCall = darknodePayment.try_darknodeBalances(darknodeAddress, zecToken);
+    if (!zecCall.reverted) { darknode.balanceZEC = zecCall.value; }
+    let bchCall = darknodePayment.try_darknodeBalances(darknodeAddress, bchToken);
+    if (!bchCall.reverted) { darknode.balanceBCH = bchCall.value; }
 
     darknode.save();
 
